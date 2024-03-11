58afe276e854a7a6df6fe55b07805dff4dd110fe
sim <simyechan87@gmail.com>
2024-03-11 17:11:37 +0900

    TypeScript로 설정 변경

---
+++ app.ts
@@ -0,0 +1,28 @@
+import { configDotenv } from 'dotenv'
+import express, { Application, Request, Response } from 'express'
+import { AppDataSoure } from './backend/src/models/dataSource';
+import cors from 'cors';
+import router from './backend/src/routes';
+
+configDotenv();
+const app: Application = express();
+const port: number = Number(process.env.PORT) || 8000;
+const server = require('http').createServer(app);
+
+// // db설정
+AppDataSoure.initialize()
+    .then(() => { console.log(`DB has initted`) })
+    .catch((err) => { console.error(err) });
+
+// cors 설정
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true
+}))
+
+// 기본 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', router);
---
+++ backend/src/models/dataSource.ts
@@ -0,0 +1,19 @@
+import { configDotenv } from "dotenv";
+import { DataSource } from "typeorm";
+import 'reflect-metadata'
+
+configDotenv();
+
+export const AppDataSoure = new DataSource({
+    type: "mysql",
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.USERNAME,
+    database: process.env.NAME,
+    password: process.env.PASSWORD,
+    synchronize: true,
+    logging: false,
+    migrations: [],
+    entities: [],
+    subscribers: [],
+});
