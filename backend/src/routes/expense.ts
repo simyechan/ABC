4a9d129d56518bd4c69befabc1e2f7b61845212f
sim <simyechan87@gmail.com>
2024-05-30 10:42:46 +0900

    인증 추가

--- backend/src/routes/expense.ts
+++ backend/src/routes/expense.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import { target, view_target, withdraw } from "../controller/expense";
+import { validationAccess } from "../controller/jwt";
 
 const router: Router = express.Router();
 
-router.post('/withdraw', withdraw);
-router.post('/target', target);
-router.get('/target', view_target);
+router.post('/withdraw', validationAccess, withdraw);
+router.post('/target', validationAccess, target);
+router.get('/target', validationAccess, view_target);
 
 export default router;
--- backend/src/routes/income.ts
+++ backend/src/routes/income.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import { deposit, goal, view_goal } from "../controller/income";
+import { validationAccess } from "../controller/jwt";
 
 const router: Router = express.Router();
 
-router.post('/deposit', deposit);
-router.post('/goal', goal);
-router.get('/goal', view_goal);
+router.post('/deposit', validationAccess, deposit);
+router.post('/goal', validationAccess, goal);
+router.get('/goal', validationAccess, view_goal);
 
 export default router;
