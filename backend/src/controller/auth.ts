5af1e8624d6f2e9d40d72aa36186beca44dad368
sim <simyechan87@gmail.com>
2024-05-30 10:42:17 +0900

    사용차 찾기 수정

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -14,7 +14,7 @@ const secertKey:string = process.env.SECRET || 'jwt-secret-key';
 const logIn = async (req:Request, res:Response) => {
   try {
     const { nick, password } = req.body;
-    const thisUser = await IsUser.findOneBy({ nick: nick, password: password });
+    const thisUser = await IsUser.findOneBy({ nick: nick });
     if (!thisUser) {
       return res.status(404).json({
         'error' : '사용자를 찾을 수 없습니다.'
